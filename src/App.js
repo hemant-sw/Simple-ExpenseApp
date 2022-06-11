8bf4c5d8b9724f2d1d6d10870cc8b88acfbae7d5
hemant-sw <hemant.ks009@gmail.com>
2022-06-11 11:00:39 +0530

    intial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpensesItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+    let expenses = [
+        {
+            id: 'e1',
+            title: 'school Fee',
+            amount: '250',
+            date: new Date(2021,5,12)
+        },
+        {
+            id: 'e2',
+            title: 'books',
+            amount: '230',
+            date: new Date(2021,7,12)
+        },
+        {
+            id: 'e3',
+            title: 'hose rent',
+            amount: '100',
+            date: new Date(2021,9,12)
+        },
+        {
+            id: 'e4',
+            title: 'milk',
+            amount: '2500',
+            date: new Date(2021,31,12)
+        }
+    ];
+
+
+
+    return ( 
+    <div>
+        <h2>Lets Go!</h2>
+        <ExpenseItem 
+            date={expenses[0].date} 
+            title ={expenses[0].title}
+            amount = {expenses[0].amount} 
+        ></ExpenseItem>
+
+         <ExpenseItem 
+            date={expenses[1].date} 
+            title ={expenses[1].title}
+            amount = {expenses[1].amount} 
+        ></ExpenseItem> 
+
+        <ExpenseItem 
+            date={expenses[2].date} 
+            title ={expenses[2].title}
+            amount = {expenses[2].amount}  
+        ></ExpenseItem>
+
+        <ExpenseItem 
+           date={expenses[3].date} 
+           title ={expenses[3].title}
+           amount = {expenses[3].amount} 
+        ></ExpenseItem>
+
+    
     </div>
-  );
+    );
 }
 
 export default App;
